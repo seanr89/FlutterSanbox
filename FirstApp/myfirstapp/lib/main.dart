c8431ed065e86225782082caa904e1f03b60b26f
Sean <srafferty89@gmail.com>
2021-08-02 21:47:53 +0100

    Got a thing to work haha with a dialog box!

--- FirstApp/myfirstapp/lib/main.dart
+++ FirstApp/myfirstapp/lib/main.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App!'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text('Hiya Sean!'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: onClick,
-          child: Text('Click'),
-        ),
-      ),
+      home: MyHome(),
     ));
 
-void onClick() {}
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App!'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Hiya Sean!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showDialog(context);
+        },
+        child: Text('Click'),
+      ),
+    );
+  }
+}
+
+_showDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: new Text("Alert!!"),
+        content: new Text("You are awesome!"),
+        actions: <Widget>[
+          new FlatButton(
+            child: new Text("OK"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
